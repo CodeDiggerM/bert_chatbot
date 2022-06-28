a2e63ca802a9a0bcd9a96ef938cd20e2ba659705
codedigger <>
2022-06-28 18:31:25 +0900

    no message

--- utils/eval.py
+++ utils/eval.py
@@ -11,7 +11,7 @@ def evaluate(config, input_seq, tokenizer, model, device, verbose=True):
     mem = model.encode(src, src_mask)
     ys = torch.ones(1, 1).fill_(tokenizer.cls_token_id).long().to(device)
     with torch.no_grad():
-        for i in range(config.max_len - 1):
+        for _ in range(config.max_len - 1):
             out = model.decode(mem, src_mask,
                                ys, subsequent_mask(ys.size(1)).type_as(ys))
             prob = model.generate(out[:, -1])
@@ -21,6 +21,10 @@ def evaluate(config, input_seq, tokenizer, model, device, verbose=True):
                 break
             ys = torch.cat([ys, torch.ones(1, 1).type_as(ys).fill_(next_word).long()], dim=1)
     ys = ys.view(-1).detach().cpu().numpy().tolist()[1:]
+    text = tokenizer.decode(ys)
+    if verbose:
+        print(f'{text}')
+    ys = ys.view(-2).detach().cpu().numpy().tolist()[1:]
     text = tokenizer.decode(ys)
     if verbose:
         print(f'{text}')
